f96e3d8de5fedadd8b15a6adcfa0eee259a76c58
Hansvi <123161387+HansviMuppala@users.noreply.github.com>
2023-11-23 18:25:52 +1100

    Add files via upload

--- main.js
+++ main.js
@@ -1,3 +1,6 @@
+leftwristX = 0;
+rightwristX = 0;
+difference = 0;
 
 function setup(){
 
@@ -11,6 +14,12 @@ poseNet = ml5.poseNet(video, modelLoaded);
 poseNet.on('pose', gotPoses);
 }
 
+function draw(){
+    textSize(difference);
+    text('Hansvi', 250, 250);
+    fill("#0f52ba");
+}
+
 function modelLoaded(){
     console.log("PoseNet is initialized!");
 }
@@ -18,5 +27,10 @@ function modelLoaded(){
 function gotPoses(){
     if(results.length > 0){
         console.log(results);
+        leftwristX = results[0].pose.leftwrist.x;
+        rightwristX = results[0].pose.rightwrist.y;
+        difference = floor(leftwristX - rightwristX);
+        
     }
 }
+
